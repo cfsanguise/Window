355ffe0cb0c88ef3cf9f2d2854bf6ff7d097130f
masterjourney <masterjourney1@gmail.comy>
2021-07-29 15:13:17 +0300

    Window version 1.2.0: Added workable forms

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,14 +1,12 @@
-const forms = () => {
+import checkNumInput from './checkNumInput';
+
+const forms = (state) => {
     const form = document.querySelectorAll('form'),
           inputs = document.querySelectorAll('input'),
           phoneInputs = document.querySelectorAll('input[name="user_phone"]');
 
 
-    phoneInputs.forEach(item => {
-        item.addEventListener('input', () => {
-            item.value = item.value.replace(/\D/, '');
-        });
-    });
+    checkNumInput('input[name="user_phone"]');
 
     const message = {
         loading: 'Загрузка...',
@@ -42,6 +40,11 @@ const forms = () => {
             item.appendChild(statusMessage);
 
             const formData = new FormData(item);
+            if (item.getAttribute('data-calc') === "end") {
+                for (let key in state) {
+                    formData.append(key, state[key]);
+                }
+            }
             postData('assets/server.php', formData)
                 .then(res => {
                     console.log(res);
